1532735d6bf2ebc88220ada336c87b12f8821ed0
Rohan <rohanluthra14@gmail.com>
2019-10-12 17:19:38 +0530

    heroku fix

--- src/helpers/mongoose.js
+++ src/helpers/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/whitepanda';
+const MONGO_URI = process.env.MONGODB_URI  || 'mongodb://localhost:27017/whitepanda';
 
 // Fixing deprication warnings
 const options = {
